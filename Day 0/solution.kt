5a0ae559423dac3c00337b0f25c37d898b1ba6cb
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2019-03-21 11:58:48 +0100

    Create solution.kt

---
+++ Day 0/solution.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+        val s = readLine()
+        println("Hello, World.")
+        println(s)
+}
