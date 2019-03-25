4274f3560aa422d88c04b7dd79532fc5af2de6c0
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2019-03-25 10:19:32 +0100

    Create solution.kt

---
+++ Day 5/solution.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    for(i in 1..10) {
+        val p = n*i
+        println("$n x $i = $p")
+    }
+}
